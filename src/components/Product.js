234511c9105f7ba446e71e595d1b6f82b1102ad5
Aleksandr1UA <uatehno@ukr.net>
2022-08-24 14:09:48 +0300

    product card

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,15 @@
+import './Product.css';
 import {Col, Card, Button} from 'react-bootstrap';
 
 function Product({product, addToCart, removeFromCart}) {
     
     return  <Col xs={6} md={4} className="my-2">
-                <Card className="p-3 h-100" border="success">
+                <Card className="p-3 h-100 product-card" border="success">
                     <Card.Img variant="top" src={product.img} />
                     <Card.Body>
                         <Card.Title className="text-center text-danger">{product.price.toFixed(2)} грн</Card.Title>
                         <Card.Text className='text-center'>{product.brand} {product.model}</Card.Text>
-                        <Card.Text className="text-center">{product.description}</Card.Text>
+                        <Card.Text className="text-center product-description">{product.description}</Card.Text>   
                     </Card.Body>
                     <div>
                         { product.addtocart ?
